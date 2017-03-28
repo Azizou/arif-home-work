6890c59cf5d6e3cddc5b171ccbca73976751246d
Azizou Ogbone <ogboneazizou@gmail.com>
2017-03-28 04:25:20 +0200

    Added solution for Question 2 without unit test

---
+++ solution_assignment_1/Question2/euler_constant.py
@@ -0,0 +1,23 @@
+# =============================================================================
+# Author: Azizou Ogbone
+# File: euler_constant.py
+# Description: A program to compute the euler's constant 
+# Date: March 28, 2017
+# Version: 1.0
+# =============================================================================
+
+import math
+
+def compute_e():
+	current_e = 0
+	old_e = -1 #Dummy value for sentinel that's different from result
+	n = 0
+	while old_e!= current_e:
+		old_e = current_e
+		current_e += 1/math.factorial(n)
+		n += 1
+	return n, current_e
+
+if __name__ == '__main__':
+	n, e = compute_e()
+	print(e)
